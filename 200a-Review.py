2d399744daa0f4304cdb6ed9aadd347f5cec7a55
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-10-13 10:29:56 -0700

    sdgdtah
    adrtjh

--- 200a-Review.py
+++ 200a-Review.py
@@ -8,30 +8,34 @@ def getIntegers(mylist):
  
     return mylist
 
-def getFactor(myList,factor):
+def getFactor(myList):
+    easy2 = myList
     # myList : expected list or tuple
     # factor : integer
     # iterate through the list and add the number to the list if
     # it is a factor of the number
-    factors = []
-
-    return factors
-
+    num = 0
+    while num < len(myList):
+       if myList[num] % 2 == 0:
+        print(myList[num], end=" ")
+        num += 1
+    
 def getNegatives(myList):
     # myList : expected list or tuple
     # iterate through myList and add all the negative numbers to the new list
-    negatives = []
-
-    return negatives
+  negatives = [x for x in myList if x <= 0 ]
+  return negatives
 
 def getIntersection(list1,list2):
-    # list 1: expected list or tuple
-    # list 2: expected list or tuple
-    # return a list of numbers that is in both lists
-    # the intersection of the 2 number sets
-    common = []
+  
+ print("The original list 1 is : " + str(list1))
+ print("The original list 2 is : " + str(list2))
+  
 
-    return common
+ res = len(set(list1) & set(list2)) / float(len(set(list1) | set(list2))) 
+  
+ return ("Percentage similarity among lists is : " + str(res))
+
 
 def getUnion(list1,list2):
     # list 1: expected list or tuple
@@ -56,9 +60,8 @@ def getMerge(list1,list2):
 def main():
     easy1 = [5,10,15,2,4,6,8]
     easy2 = [-2,-4,-6,2,4,6,0.1]
-    numbers1 = [3,5,8,12,11,19,10,7,2,15,25,34,16,32,50,60,100,-3,0.25]
-    numbers2 = [3,7,11,15,19,23,27,31,35,39,44,50]
-    print(getIntegers(easy2))
-
+    list1 = [3,5,8,12,11,19,10,7,2,15,25,34,16,32,50,60,100,-3,0.25]
+    list2 = [3,7,11,15,19,23,27,31,35,39,44,50]
+    print(getIntersection(list1,list2))
 if __name__ == "__main__":
     main()
